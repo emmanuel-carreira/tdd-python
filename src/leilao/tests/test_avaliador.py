3bf0df198fc59acc02662b21c7f1dedd556cfc28
Emmanuel Carreira Alves <eca2@cesar.org.br>
2021-07-20 15:11:12 +0000

    feat: adds setUp() routine and new test methods

--- src/leilao/tests/test_avaliador.py
+++ src/leilao/tests/test_avaliador.py
@@ -5,21 +5,65 @@ from src.leilao.dominio import Avaliador, Lance, Leilao, Usuario
 
 class TestAvaliador(TestCase):
 
-    def test_avalia(self):
-        usuario = Usuario(nome="Usuário teste")
+    def setUp(self):
+        self.usuario_teste = Usuario(nome="Usuário teste")
+        self.usuario_teste_2 = Usuario(nome="Usuário teste 2")
 
-        lance = Lance(usuario=usuario, valor=200.00)
-        lance_2 = Lance(usuario=usuario, valor=150.00)
+        self.lance_150 = Lance(usuario=self.usuario_teste, valor=150.00)
+        self.lance_200 = Lance(usuario=self.usuario_teste_2, valor=200.00)
 
-        leilao = Leilao(descricao="Leilão teste")
-        leilao.lances.append(lance)
-        leilao.lances.append(lance_2)
+        self.leilao = Leilao(descricao="Leilão teste")
 
-        avaliador = Avaliador()
-        avaliador.avalia(leilao=leilao)
+        self.avaliador = Avaliador()
+
+    def test_avalia_lances_crescentes(self):
+        self.leilao.lances.append(self.lance_150)
+        self.leilao.lances.append(self.lance_200)
+
+        self.avaliador.avalia(leilao=self.leilao)
 
         maior_lance_esperado = 200.00
         menor_lance_esperado = 150.00
 
-        self.assertEqual(maior_lance_esperado, avaliador.maior_lance)
-        self.assertEqual(menor_lance_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_lance_esperado, self.avaliador.maior_lance)
+        self.assertEqual(menor_lance_esperado, self.avaliador.menor_lance)
+
+
+    def test_avalia_lances_decrescentes(self):
+        self.leilao.lances.append(self.lance_200)
+        self.leilao.lances.append(self.lance_150)
+
+        self.avaliador.avalia(leilao=self.leilao)
+
+        maior_lance_esperado = 200.00
+        menor_lance_esperado = 150.00
+
+        self.assertEqual(maior_lance_esperado, self.avaliador.maior_lance)
+        self.assertEqual(menor_lance_esperado, self.avaliador.menor_lance)
+
+    def test_avalia_um_lance(self):
+        self.leilao.lances.append(self.lance_200)
+
+        self.avaliador.avalia(leilao=self.leilao)
+
+        maior_lance_esperado = 200.00
+        menor_lance_esperado = 200.00
+
+        self.assertEqual(maior_lance_esperado, self.avaliador.maior_lance)
+        self.assertEqual(menor_lance_esperado, self.avaliador.menor_lance)
+
+    def test_avalia_lances_aleatorios(self):
+        self.leilao.lances.append(self.lance_200)
+        self.leilao.lances.append(self.lance_150)
+
+        usuario_teste_3 = Usuario("Usuario teste 3")
+        lance_300 = Lance(usuario=usuario_teste_3, valor=300.00)
+        self.leilao.lances.append(lance_300)
+
+        self.avaliador.avalia(leilao=self.leilao)
+
+        maior_lance_esperado = 300.00
+        menor_lance_esperado = 150.00
+
+        self.assertEqual(maior_lance_esperado, self.avaliador.maior_lance)
+        self.assertEqual(menor_lance_esperado, self.avaliador.menor_lance)
